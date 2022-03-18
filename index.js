a7054ec90e3b78d3b7d62a4331a305aa8918d1ca
KevinScheffler <schefk2122@gmail.com>
2022-03-18 09:16:49 -0400

    finished and refactored getInitials

---
+++ index.js
@@ -0,0 +1,27 @@
+// Get Initials
+
+// const getInitials = (firstName, lastName) => {
+//   const first = firstName.slice(0, 1);
+//   const last = lastName.slice(0, 1);
+//   return `${first.toUpperCase()}.${last.toUpperCase()}`;
+// }
+
+// const getInitials = (name) => {
+//   const names = name.split(' ');
+//   const initials = `${names[0][0].toUpperCase()}.${names[1][0].toUpperCase()}`;
+//   return initials;
+// }
+
+// console.log(getInitials('Aaron Jack'));
+// console.log(getInitials('Jan Frey'));
+// console.log(getInitials('Harry Potter'));
+
+// function getInitials(name) {
+//   // split the input at " " -> ["Aaron", "Jack"]
+//   var names = name.split(' ');
+//   var initials = `${names[0][0]}.${names[1][0]}`;
+//   return initials;
+// }
+
+
+// console.log(getInitials("Aaron Jack"))
