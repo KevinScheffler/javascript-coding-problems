608af914cb6629a10908e1ac67116f92c49bfc0a
KevinScheffler <schefk2122@gmail.com>
2022-03-20 14:15:56 -0400

    finished sumOfPositive problem

--- index.js
+++ index.js
@@ -25,3 +25,28 @@
 
 
 // console.log(getInitials("Aaron Jack"))
+
+
+
+
+
+
+
+// sumOfPositive
+
+const sumOfPositive = nums => {
+  let sum = 0;
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] > 0) {
+      sum += nums[i];
+    }
+  }
+  return sum;
+}
+
+
+
+
+console.log(sumOfPositive([1,-2,3,4]));
+console.log(sumOfPositive([-2,-5,-10,-1]));
+console.log(sumOfPositive([4,40,-40,-4]));
